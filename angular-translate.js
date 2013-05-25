91ed20b30688f9f4303dcdbe696fc6ab1dba3cc8
Pascal Precht <pascal.precht@googlemail.com>
2013-05-25 12:53:39 +0200

    update to 0.9.1

--- angular-translate.js
+++ angular-translate.js
@@ -231,11 +231,3 @@ angular.module('pascalprecht.translate').filter('translate', [
     };
   }
 ]);
-angular.module('pascalprecht.translate').factory('$translateMissingTranslationHandlerLog', [
-  '$log',
-  function ($log) {
-    return function (translationId) {
-      $log.warn('Translation for ' + translationId + ' doesn\'t exist');
-    };
-  }
-]);
