da341132e116dddce754a4f40657670558278a14
Pascal Precht <pascal.precht@googlemail.com>
2013-07-26 09:07:05 +0200

    releases 1.0.1

--- angular-translate.js
+++ angular-translate.js
@@ -23,6 +23,22 @@ angular.module('pascalprecht.translate', ['ng']).run([
   }
 ]);
 angular.module('pascalprecht.translate').constant('$STORAGE_KEY', 'NG_TRANSLATE_LANG_KEY');
+angular.module('pascalprecht.translate').factory('$translateDefaultInterpolation', [
+  '$interpolate',
+  function ($interpolate) {
+    var $translateInterpolator = {}, $locale, $identifier = 'default';
+    $translateInterpolator.setLocale = function (locale) {
+      $locale = locale;
+    };
+    $translateInterpolator.getInterpolationIdentifier = function () {
+      return $identifier;
+    };
+    $translateInterpolator.interpolate = function (string, interpolateParams) {
+      return $interpolate(string)(interpolateParams);
+    };
+    return $translateInterpolator;
+  }
+]);
 angular.module('pascalprecht.translate').provider('$translate', [
   '$STORAGE_KEY',
   function ($STORAGE_KEY) {
