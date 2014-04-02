0b9e64225c96458aa6bc00bd271c78353584d7c0
Pascal Precht <pascal.precht@googlemail.com>
2014-04-02 21:49:06 +0200

    fixes comment

--- angular-translate.min.js
+++ angular-translate.min.js
@@ -1,5 +1,5 @@
 /*!
- * angular-translate - v2.0.1 - 2014-04-02
+ * angular-translate - v2.1.0 - 2014-04-02
  * http://github.com/PascalPrecht/angular-translate
  * Copyright (c) 2014 ; Licensed MIT
  */
